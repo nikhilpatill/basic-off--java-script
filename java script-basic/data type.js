543a6b40c367c42c0f6d5292e6d278e1b428e155
pune darshan <nikhilpatil1050@gmail.com>
2021-10-11 16:03:10 +0530

    null and undefine check

--- java script-basic/data type.js
+++ java script-basic/data type.js
@@ -12,3 +12,17 @@ console.log(" nikhil "- "patil")// nan not anumber
 console.log(true + true)//   1+1=2
 console.log( true +false)// 1+0=1
 console.log( false + false )// 0+0=0
+
+//   interview question 1
+//  difference betweeen null vs  undefine //
+
+var name=null;
+console.log(name) // null
+console.log(typeof(name)) // object this is bugg
+
+var name1;
+console.log(name1); // undefine 
+console.log(typeof(name1))// undefinee 
+
+
+
