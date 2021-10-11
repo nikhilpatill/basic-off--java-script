925dcd2120ea359aa9040c9024c1220230024b76
pune darshan <nikhilpatil1050@gmail.com>
2021-10-11 15:47:28 +0530

    example

---
+++ java script-basic/data type.js
@@ -0,0 +1,14 @@
+
+console.log(10 + 20) // ans 20
+console.log(9-"5")// 4
+console.log(" java "+ "java") // java java 
+console.log(" " +" ") // 
+console.log(" ", 0)//  0
+
+console.log( "nikhil "+ "patil") // nikhil 
+console.log(" nikhil "- "patil")// nan not anumber 
+
+//true=>1 false=> 0;
+console.log(true + true)//   1+1=2
+console.log( true +false)// 1+0=1
+console.log( false + false )// 0+0=0
--- java script-basic/value_variable.js
+++ java script-basic/value_variable.js
@@ -1,4 +1,27 @@
 //*** section 3 value and variable  */
+// variable => veriable is container name to stire some value .
 
 var my =" nikhil";
-console(my);
+console.log(my)
+
+// ex 1.  var        name =    value 
+ //     (kay name)
+
+ var name=10;  //10
+ console.log(" nikhil=>"+name)
+
+ 
+  var $log=10  //10
+ console.log($log);
+
+ var name="nikhil"//Sting 
+ var num=10;  // number 
+ var fce=false;
+ 
+ console.log(typeof(name))
+ console.log(typeof(num))
+console.log(typeof(name))
+
+ 
+
+
