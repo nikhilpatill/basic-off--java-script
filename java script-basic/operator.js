b3e105fb82c4fe1f893733d51f038b683980c5fd
pune darshan <nikhilpatil1050@gmail.com>
2021-10-11 16:55:12 +0530

    typeoff is data type ?

---
+++ java script-basic/operator.js
@@ -0,0 +1,7 @@
+
+// arithematic operator (+,-,*,/,++,--)
+// ralational operator or comparisum oprtaor (==, ===, !=, >,<,>=,<=)
+// bitwise operator (&  and ,| or)
+// logical operator (&&,||)
+// assignment operator (+=,-=,*=./=)
+// special operator (?:, delete, in , typeof )
