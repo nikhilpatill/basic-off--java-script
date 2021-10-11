d7ffaf359556aa104ea98e94bb7ccbe4f6720c02
pune darshan <nikhilpatil1050@gmail.com>
2021-10-11 13:02:43 +0530

    print hello word

---
+++ java script-basic/hello.js
@@ -0,0 +1,8 @@
+
+// different type off of the write hwllo word 
+
+//   use consol.log //
+// when we debbug the code and website if any mistake come to be there that time we inpect the website and check it .
+// which function not call properly not work properly use consol
+
+console.log("hello word ")
